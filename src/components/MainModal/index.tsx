b1d83a3085c33035afc6c75ece01558cef0e32af
Leonardo Santos <kuhnnhuk@outlook.com>
2023-03-08 08:43:46 -0300

    feat: set new state, new request and starts 'see tasks' logic

--- src/components/MainModal/index.tsx
+++ src/components/MainModal/index.tsx
@@ -3,10 +3,16 @@ import { AdminContext } from "../../contexts/AdminContext";
 import { DivMainModal, DivMainModalWrapper } from "./styles";
 
 const MainModal = () => {
-  const { setModal, modal, idButton, users } = useContext(AdminContext);
+  const { setModal, modal, idButton, users, getTaskById, tasks } =
+    useContext(AdminContext);
 
   const closeModal = () => {
     setModal(false);
+  };
+
+  const seeTasks = (id: number) => {
+    getTaskById(id - 1);
+    console.log(tasks);
   };
 
   const user = users?.find((user) => user.id == idButton);
@@ -17,22 +23,26 @@ const MainModal = () => {
         <DivMainModalWrapper>
           <DivMainModal>
             {user && (
-              <header key={crypto.randomUUID()}>
-                <div>
-                  <h2>{user.name}</h2>
-                  <p>{user.office}</p>
-                  <span>{user.shift}</span>
-                </div>
-                <button onClick={closeModal}>fechar</button>
-              </header>
-            )}
+              <>
+                <header key={crypto.randomUUID()}>
+                  <div>
+                    <h2>{user.name}</h2>
+                    <p>{user.office}</p>
+                    <span>{user.shift}</span>
+                  </div>
+                  <button onClick={closeModal}>fechar</button>
+                </header>
 
-            <div>
-              <button>Ver tarefas atribuídas</button>
-              <button>Incluir tarefa</button>
-              <button>Ver folha ponto</button>
-              <button>Excluir usuário</button>
-            </div>
+                <div>
+                  <button onClick={() => seeTasks(user.id)}>
+                    Ver tarefas atribuídas
+                  </button>
+                  <button>Incluir tarefa</button>
+                  <button>Ver folha ponto</button>
+                  <button>Excluir usuário</button>
+                </div>
+              </>
+            )}
           </DivMainModal>
         </DivMainModalWrapper>
       )}
